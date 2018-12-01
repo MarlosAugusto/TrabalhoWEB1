ca14428913f900f7bfa2ee65545310d5104e6995
NathanWeber <42284129+NathanWeber@users.noreply.github.com>
2018-12-01 16:33:57 -0200

    Criado script
    Criado script para aumentar e diminuir quantidade de compra, porém está funcionando para só um card

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -1,2 +1,24 @@
 
 // mapa de imagem
+
+/*unction incrementaValor(valorMaximo){
+	var value = parseInt(document.getElementById('resultado').value,10);
+  value = isNaN(value) ? 0 : value;
+  if(value >= valorMaximo) {
+    value = valorMaximo;
+  }else{
+    value++;
+  }
+  document.getElementById('resultado').value = value;
+}
+
+function decrementaValor(valorMinimo){
+  var value = parseInt(document.getElementById('resultado').value,10);
+  value = isNaN(value) ? 0 : value;
+  if(value <= valorMinimo) {
+    value = 0;
+  }else{
+    value--;
+  }
+  document.getElementById('resultado').value = value;
+}*/
