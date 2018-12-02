773b9d14a39a542e2bb2ba37d052ab1a95dff769
MarlosAugusto <marlosaugusto@outlook.com>
2018-12-02 17:42:10 -0200

    mapeamento completo

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -1,24 +1,38 @@
 
-// mapa de imagem
+var value;
 
-/*unction incrementaValor(valorMaximo){
-	var value = parseInt(document.getElementById('resultado').value,10);
-  value = isNaN(value) ? 0 : value;
-  if(value >= valorMaximo) {
-    value = valorMaximo;
-  }else{
-    value++;
-  }
-  document.getElementById('resultado').value = value;
+function incrementaValor(valorMaximo, id){
+    value = parseInt(document.getElementById(id).value,10);
+    value = isNaN(value) ? 0 : value;
+    if(value >= valorMaximo) {
+        value = valorMaximo;
+    }else{
+        value++;
+    }
+    document.getElementById(id).value = value;
 }
 
-function decrementaValor(valorMinimo){
-  var value = parseInt(document.getElementById('resultado').value,10);
-  value = isNaN(value) ? 0 : value;
-  if(value <= valorMinimo) {
-    value = 0;
-  }else{
-    value--;
-  }
-  document.getElementById('resultado').value = value;
-}*/
+function decrementaValor(valorMinimo, id){
+    value = parseInt(document.getElementById(id).value,10);
+    value = isNaN(value) ? 0 : value;
+    if(value <= valorMinimo) {
+        value = 0;
+    }else{
+        value--;
+    }
+    document.getElementById(id).value = value;
+}
+
+// alert mapa
+/*
+$('area').on('click', function() {
+    alert($(this).attr('alt') + ' clicked');
+});*/
+
+
+//mapa
+
+function mudaFoto(foto) {
+    document.getElementById("mapa-index").src = foto;
+
+}
