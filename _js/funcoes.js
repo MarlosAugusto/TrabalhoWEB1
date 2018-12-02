7adb8f63670fd19449a1c529d32181aa3ac54526
MarlosAugusto <marlosaugusto@outlook.com>
2018-12-02 21:01:20 -0200

    add funçoes de personalização - incompleto

--- _js/funcoes.js
+++ _js/funcoes.js
@@ -32,7 +32,33 @@ $('area').on('click', function() {
 
 //mapa
 
-function mudaFoto(foto) {
+function mudaFoto(foto, id) {
     document.getElementById("mapa-index").src = foto;
+    alert(id)
+    $( ".add" ).remove();
+    if (id = 'pan') {
+        img = "http://pensenegocios.com.br/wp-content/uploads/2017/05/vendo-mercado-e-padaria.jpg"
+        legenda = "<h3 class='add'>Panificadora</h3><p class='add'>Venha conhecer nossa maravilhosa panificadora!</p>"
+    }
+    document.getElementById("img-legenda").src = img
+    $( "#legenda" ).append( legenda );
 
 }
+
+//frame
+
+$(document).ready(function() {
+    if (window.innerWidth < 600) {
+        document.getElementById("foto-legenda").style.width= '395px'
+    } else {
+        document.getElementById("foto-legenda").style.width = '595px'
+    }
+})
+
+window.addEventListener('resize', function(){
+    if (window.innerWidth < 600) {
+        document.getElementById("foto-legenda").style.width= '395px'
+    } else {
+        document.getElementById("foto-legenda").style.width = '595px'
+    }
+})
